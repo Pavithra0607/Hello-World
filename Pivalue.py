9daf6a769a131ce9f95dbcf54af252eb0478288e
Pavithra0607 <46147757+Pavithra0607@users.noreply.github.com>
2018-12-26 12:47:21 +0530

    Update Pivalue.py

--- Pivalue.py
+++ Pivalue.py
@@ -1,8 +1,19 @@
 '''
 Name: Pivalue.py
 Purpose: To display pi value upto n digits entered by user
-input: An integer number
+input: An integer upto 50
 '''
 from math import pi
-number_of_places = int(input("Please enter the number of places you want to see after decimal point: "))
-print("Pi value upto {} digit is {:.{}f}".format(number_of_places,pi,number_of_places))
+while True:
+    try:
+        number_of_places = int(input("Please enter the number of places you want to see after decimal point: "))
+    except:
+        print("Please enter a correct number!")
+        continue
+    else:
+        if number_of_places > 50:
+            print("Number till 50 is only allowed!")
+            continue
+        else:
+            print("Pi value upto {} digit is {:.{}f}".format(number_of_places,pi,number_of_places))
+            break
