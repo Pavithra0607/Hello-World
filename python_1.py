abab80627db9f2c1363b38b7fb1a9247f44c8e95
Madhusoodan <Madhusoodan@users.noreply.github.com>
2019-07-17 12:51:37 +0530

    Add files via upload

--- python_1.py
+++ python_1.py
@@ -5,3 +5,13 @@ one='spam eggs '
 two="doesn't"
 print(one+two)
 #concatenate
+print('Isn\'t?, they said.')
+#nextone lol. Strings can be concatenated (glued together) with the + operator, and repeated with *:
+print (3 * 'un' + 'ium')
+concat= (3 * 'un' + 'ium');
+#pritn the placeholder value
+print (concat[1])
+print (concat[-1])
+#lists now
+squares = [1, 4, 9, 16, 25]
+print(squares)
